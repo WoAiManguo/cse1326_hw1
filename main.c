fd8e5920a9d67e668a2eb170208efd9cbd267735
WoAiManguo <il242775@gmail.com>
2025-08-27 15:20:50 -0500

    removed isdigit check

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #include <ctype.h>
 
 
-int main ()
+int main (int argc, char *argv[])
 {
-    char u_in[50];
+    char u_in[] = "";
     char* pend;
     float lat;
     float lon;
@@ -20,14 +20,16 @@ int main ()
         return 1;
     }
 
+    fclose(file);
+
 
     while (strcmp(u_in, "stop") != 0)
     {
         scanf("%[^\n]%*c", u_in);
         printf("string: %s\n", u_in);
-        if(isdigit(u_in) != 0)
+        lat = strtof(u_in, &pend);
+        if (pend != u_in)
         {
-            lat = strtof(u_in, &pend);
             lon = strtof(pend, NULL);
             printf("lat is %f lon is %f\n", lat, lon);
         }
